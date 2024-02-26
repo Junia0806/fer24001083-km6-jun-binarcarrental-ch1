81041be26653fae073219990563988260d5ba294
Junia0806 <juniavitasari0806@gmail.com>
2024-02-26 12:15:34 +0700

    landing page

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export const theme = {
+  extend: {
+    colors: {
+      'custom-color': '#5CB85F',
+      'custom-color-dark': '#4C9951',
+      'custom-color-light': '#7FD179',
+    },
+  },
+};
+export const variants = {};
+export const plugins = [];
